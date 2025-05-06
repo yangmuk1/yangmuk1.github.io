68a9e589fcde10f5c973d8603bb086ed020b1d6b
yangmuk1 <hjs040304@snu.ac.kr>
2025-05-06 08:13:39 +0000

    Deploying to gh-pages from @ yangmuk1/yangmuk1.github.io@90b3f5981246e3d68a7cf27386aee3141592fa9a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,7 +72,18 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-나의-첫-jekyll-블로그-포스트",
+            },{id: "post-markdown-문법-기초",
+        
+          title: "Markdown 문법 기초",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/markdown/";
+          
+        },
+      },{id: "post-나의-첫-jekyll-블로그-포스트",
         
           title: "나의 첫 Jekyll 블로그 포스트",
         
