ba73e54be4967ec2a31cdfd2b7c127a8e40902be
yangmuk1 <hjs040304@snu.ac.kr>
2025-05-06 06:02:12 +0000

    Deploying to gh-pages from @ yangmuk1/yangmuk1.github.io@6f22058b3f9c5255492e79c4f07cbb65c515b207 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,7 +72,18 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-a-post-with-plotly-js",
+            },{id: "post-",
+        
+          title: "",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/2025-04-30-my-first-jekyll-blog-post/";
+          
+        },
+      },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
